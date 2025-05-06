3e6e71d5a9b78e68aa951a910f2ac61210245808
mafindijr <abdulrazaqiliyasumafindi@gmail.com>
2025-05-06 05:16:32 -0700

    added function to handle the value of the text field

--- src/components/edu-experience.jsx
+++ src/components/edu-experience.jsx
@@ -5,4 +5,16 @@ export default function EduExperience () {
     const [school, setSchool] = useState("");
     const [leveOfStudy, setLevelOfStudy] = useState("");
     const [dateOfStudy, setDateOfStudy] = useState("");
+
+    function handleSchoolChange(e) {
+        setSchool(e.target.value);
+    }
+
+    function handleLevelChange(e) {
+        setLevelOfStudy(e.target.value);
+    }
+
+    function handleDateChange(e) {
+        setDateOfStudy(e.target.value);
+    }
 }
