271c4170052c5400c6de74368996555f127b13e3
mafindijr <abdulrazaqiliyasumafindi@gmail.com>
2025-05-10 02:29:01 -0700

    added some text field the component

--- src/components/edu-experience.jsx
+++ src/components/edu-experience.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 
 export default function EduExperience ( {onChange} ) {
 
-    const [schoolInfor, setSchoolInfor] = useState({school: "", levelOfStudy: "", dateOfStudy: ""});
+    const [schoolInfor, setSchoolInfor] = useState({ school: "", levelOfStudy: "", dateOfStudy: "" });
     
     const handleSchoolChange = (e) => {
         const { name, value } = e.target;
@@ -26,6 +26,32 @@ export default function EduExperience ( {onChange} ) {
                     name="school"
                     placeholder="school name"
                     value={schoolInfor.school}
+                    onChange={handleSchoolChange}
+                />
+            </label>
+
+            <label htmlFor="level">
+
+                Leve of study {" "}
+
+                <input
+                    type="text"
+                    name="level of study"
+                    placeholder="level of study"
+                    value={schoolInfor.levelOfStudy}
+                    onChange={handleSchoolChange}
+                />
+            </label>
+
+            <label htmlFor="date">
+
+                Date {" "}
+
+                <input
+                    type="text"
+                    name="Date"
+                    placeholder="Date"
+                    value={schoolInfor.dateOfStudy}
                     onChange={handleSchoolChange}
                 />
             </label>
